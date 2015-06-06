fe89bc246735a94e438ad31e385b7d9f820b0371
Chris Archibald <carchi8py@gmail.com>
2015-06-05 18:45:20 -0700

    Added background to app

--- Tetris/GameScene.swift
+++ Tetris/GameScene.swift
@@ -23,7 +23,7 @@ class GameScene: SKScene {
         
         anchorPoint = CGPoint(x: 0, y: 1.0)
         
-        let background = SKSpriteNode(imageNamed: "backgound")
+        let background = SKSpriteNode(imageNamed: "background")
         background.position = CGPoint(x: 0, y: 0)
         background.anchorPoint = CGPoint(x: 0, y: 1.0)
         addChild(background)
--- Tetris/GameViewController.swift
+++ Tetris/GameViewController.swift
@@ -12,8 +12,23 @@ import SpriteKit
 
 class GameViewController: UIViewController {
 
+    var scene: GameScene!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+       
+        
+        //Configure the view
+        let skView = self.view as! SKView
+        skView.multipleTouchEnabled = false
+        
+        //Create and configure the Scene.
+        
+        scene = GameScene(size: skView.bounds.size)
+        scene.scaleMode = .AspectFill
+        
+        //Present the scene.
+        skView.presentScene(scene)
 
     }
 
