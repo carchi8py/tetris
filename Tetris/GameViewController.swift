9933791779209db2b3239f7887c8bd6a5e69b2d9
Chris Archibald <carchi8py@gmail.com>
2015-06-05 21:29:55 -0700

    A Shape will now fall from the sky

--- Tetris/GameViewController.swift
+++ Tetris/GameViewController.swift
@@ -13,6 +13,7 @@ import SpriteKit
 class GameViewController: UIViewController {
 
     var scene: GameScene!
+    var tetris: Tetris!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -27,12 +28,30 @@ class GameViewController: UIViewController {
         scene = GameScene(size: skView.bounds.size)
         scene.scaleMode = .AspectFill
         
+        scene.tick = didTick
+        
+        tetris = Tetris()
+        tetris.beginGame()
+        
         //Present the scene.
         skView.presentScene(scene)
-
+        
+        scene.addPreviewShapeToScene(tetris.nextShape!) {
+            self.tetris.nextShape?.moveTo(StartingColumn, row: StartingColumn)
+            self.scene.movePreviewShape(self.tetris.nextShape!) {
+                let nextShapes = self.tetris.newShape()
+                self.scene.startTicking()
+                self.scene.addPreviewShapeToScene(nextShapes.nextShape!) {}
+            }
+        }
     }
 
     override func prefersStatusBarHidden() -> Bool {
         return true
+    }
+    
+    func didTick() {
+        tetris.fallingShape?.lowerShapeByOneRow()
+        scene.redrawShape(tetris.fallingShape!, completion: {})
     }
 }
